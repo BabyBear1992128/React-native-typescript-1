ccabd0fd28d971d86d440a2577c02ea56d45dbe6
Baby Pooh <babybearbear1280@gmail.com>
2023-01-08 07:56:30 -0500

    add favorite component

--- src/screens/favorite/favorite-screen.tsx
+++ src/screens/favorite/favorite-screen.tsx
@@ -1,81 +1,97 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import { COMMON_STYLES } from "~theme/common-styles";
 
 function FavoriteScreen() {
   return (
-    <View style={style.container}>
-      <Text style={style.title}>Meal Name</Text>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={style.container}>
+          <Text style={style.title}>Meal Name</Text>
 
-      <View style={style.title_container}>
-        <Text style={style.title}>Appetizer</Text>
+          <View style={style.title_container}>
+            <Text style={style.title}>Appetizer</Text>
 
-        <FontAwesome5
-            name="plus"
-            style={COMMON_STYLES.COMMON_ICON_STYLES}
-        />
-      </View>
-
-
-      <View style={style.item_container}>
-        <TouchableOpacity style={style.touchable_container}>
-          <View style={style.item}>
-            <Text style={style.item_text}>Teriyaki Pineapple Meatballs</Text>
             <FontAwesome5
-              name="pen"
+              name="plus"
               style={COMMON_STYLES.COMMON_ICON_STYLES}
             />
           </View>
-        </TouchableOpacity>
 
-        <View style={{ height: 5 }} />
+          <View style={style.item_container}>
+            <TouchableOpacity style={style.touchable_container}>
+              <View style={style.item}>
+                <Text style={style.item_text}>
+                  Teriyaki Pineapple Meatballs
+                </Text>
+                <FontAwesome5
+                  name="pen"
+                  style={COMMON_STYLES.COMMON_ICON_STYLES}
+                />
+              </View>
+            </TouchableOpacity>
 
-        <TouchableOpacity style={style.touchable_container}>
-          <View style={style.item}>
-            <Text style={style.item_text}>Fruit Charcuterie Board</Text>
+            <View style={{ height: 5 }} />
+
+            <TouchableOpacity style={style.touchable_container}>
+              <View style={style.item}>
+                <Text style={style.item_text}>Fruit Charcuterie Board</Text>
+                <FontAwesome5
+                  name="pen"
+                  style={COMMON_STYLES.COMMON_ICON_STYLES}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={style.title_container}>
+            <Text style={style.title}>Entree</Text>
+
             <FontAwesome5
-                name="pen"
-                style={COMMON_STYLES.COMMON_ICON_STYLES}
+              name="plus"
+              style={COMMON_STYLES.COMMON_ICON_STYLES}
             />
           </View>
-        </TouchableOpacity>
-      </View>
 
-      <View style={style.title_container}>
-        <Text style={style.title}>Entree</Text>
+          <View style={style.item_container}>
+            <TouchableOpacity style={style.touchable_container}>
+              <View style={style.item}>
+                <Text style={style.item_text}>
+                  Chorizo & mozzarella gnocchi bake
+                </Text>
+                <FontAwesome5
+                  name="pen"
+                  style={COMMON_STYLES.COMMON_ICON_STYLES}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
 
-        <FontAwesome5
-            name="plus"
-            style={COMMON_STYLES.COMMON_ICON_STYLES}
-        />
-      </View>
+          <View style={style.title_container}>
+            <Text style={style.title}>Dessert</Text>
 
-
-      <View style={style.item_container}>
-        <TouchableOpacity style={style.touchable_container}>
-          <View style={style.item}>
-            <Text style={style.item_text}>Chorizo & mozzarella gnocchi bake</Text>
             <FontAwesome5
-                name="pen"
-                style={COMMON_STYLES.COMMON_ICON_STYLES}
+              name="plus"
+              style={COMMON_STYLES.COMMON_ICON_STYLES}
             />
           </View>
-        </TouchableOpacity>
-      </View>
 
+          <View style={{ flexGrow: 1 }} />
 
-
-      <View style={style.title_container}>
-        <Text style={style.title}>Dessert</Text>
-
-        <FontAwesome5
-            name="plus"
-            style={COMMON_STYLES.COMMON_ICON_STYLES}
-        />
-      </View>
-    </View>
+          <View style={style.item_container}>
+            <TouchableOpacity style={style.touchable_container_blue}>
+              <View style={style.item_center}>
+                <Text style={style.item_text_white}>
+                  SET THE DATE!
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -106,13 +122,19 @@ const style = StyleSheet.create({
     textAlign: "left",
     fontWeight: "bold",
     marginTop: 10,
-    paddingLeft: 10
+    paddingLeft: 10,
   },
   item_text: {
     marginLeft: 20,
     fontSize: 15,
     fontWeight: "bold",
     color: "#555d6b",
+  },
+  item_text_white: {
+    marginLeft: 20,
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "white",
   },
   sub_item: {
     flexDirection: "row",
@@ -132,9 +154,28 @@ const style = StyleSheet.create({
     shadowRadius: 5,
     elevation: 7,
   },
+  touchable_container_blue: {
+    backgroundColor: "#2e9dfb",
+    color: "white",
+    borderRadius: 200,
+    paddingVertical: 20,
+    paddingRight: 20,
+    width: "100%",
+    marginVertical: 5,
+    shadowColor: "#2f394a",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    elevation: 7,
+  },
   item: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+  },
+  item_center: {
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
   },
 });
